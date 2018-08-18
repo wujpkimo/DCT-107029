828daf58da58dd261094dc908b32c0ffbd6ee910
Will <doggy.huang@gmail.com>
2018-08-18 21:17:01 +0800

    完成 Entity Framework 基本 CRUD 開發

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleApp1.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,60 @@ namespace ConsoleApp1
 {
     class Program
     {
+        static int InsertedId;
+
         static void Main(string[] args)
         {
+            using (var db = new ContosoUniversityEntities())
+            {
+                QueryCourse(db);
+
+                InsertDepartment(db);
+
+                UpdateDepartment(db);
+
+                RemoveDepartment(db);
+            }
+        }
+
+        private static void InsertDepartment(ContosoUniversityEntities db)
+        {
+            var dept = new Department()
+            {
+                Name = "Will",
+                Budget = 100,
+                StartDate = DateTime.Now
+            };
+
+            db.Department.Add(dept);
+            db.SaveChanges();
+
+            InsertedId = dept.DepartmentID;
+        }
+
+        private static void UpdateDepartment(ContosoUniversityEntities db)
+        {
+            var dept = db.Department.Find(InsertedId);
+            dept.Name = "John";
+            db.SaveChanges();
+        }
+
+        private static void RemoveDepartment(ContosoUniversityEntities db)
+        {
+            db.Department.Remove(db.Department.Find(InsertedId));
+            db.SaveChanges();
+        }
+
+        private static void QueryCourse(ContosoUniversityEntities db)
+        {
+            var data = from p in db.Course select p;
+
+            foreach (var item in data)
+            {
+                Console.WriteLine(item.CourseID);
+                Console.WriteLine(item.Title);
+                Console.WriteLine();
+            }
         }
     }
 }
